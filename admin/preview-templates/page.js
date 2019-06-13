7c20cf481e3c20287723d0c63f29cb2557d80d11
lewisnyman <hello@lewisnyman.co.uk>
2019-06-13 12:26:38 +0100

    Add subtitle to live preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -10,6 +10,7 @@ const Page = createClass({
     return html`
       <main>
         <h1>${entry.getIn(["data", "title"], null)}</h1>
+        <h2 class="subtitle">>${entry.getIn(["data", "subtitle"], null)}</h2>
 
         ${this.props.widgetFor("body")}
       </main>
