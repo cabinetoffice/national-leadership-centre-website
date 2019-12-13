b0a8049c29788ec735a2ce14348c02727acc917c
Sam Medhurst <sammedhurst777@gmail.com>
2019-12-13 10:50:08 +0000

    Further build of navigation bar. Styling and scripts for functionality written. CSS units defined in pixels for future-proofing, with the planned merge in of a px to rem govuk helper. [ch886].

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,20 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+$(document).ready(function(){
+	// Open and close loop for main navigation on mobile.
+	$(".header-main__menu").click(function(){ // when user clicks on nav btn..
+		if ($('.open-mobile').length) { // .. if nav is already open..
+			$(".header-main").removeClass('open-mobile'); // .. remove the class..
+			$('.js-open-text').hide(); // .. hide 'closed' text..
+			$('.js-closed-text').show(); // .. display 'open' text..
+		}
+		else { // .. if not ..
+			$(".header-main").addClass('open-mobile'); // .. add the open on mobile class..
+			$('.js-open-text').css('display','inline-block'); // .. display 'close' text..
+			$('.js-closed-text').hide(); // .. hide 'open' text..
+		}
+	});
+});
+  
