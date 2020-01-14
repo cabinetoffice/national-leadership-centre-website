5de7f09a1c9c709abb24673fde6339a12097c343
Sam Medhurst <sammedhurst777@gmail.com>
2020-01-14 18:07:56 +0100

    Amending cookie bar. Still need to block GA cookies before user accepts cookies. [ch1167]

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -8,6 +8,55 @@ if (window.netlifyIdentity) {
   });
 }
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+	if (getCookie('nlc_cookie_bar').length > 0) {
+	  document.getElementById('cookie-bar-bottom').style.display = 'none';
+	}
+	document.getElementById('cookie-hide').addEventListener('click', function(e) {
+	  createCookie('nlc_cookie_bar', true, 1);
+	  document.getElementById('cookie-bar-bottom').style.display = 'none';
+	})
+});
+  
+  
+  
+  var createCookie = function(name, value, days) {
+	var expires;
+	if (days) {
+	  var date = new Date();
+	  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+	  expires = "; expires=" + date.toGMTString();
+	}
+	else {
+	  expires = "";
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+  }
+  
+  function getCookie(c_name) {
+	if (document.cookie.length > 0) {
+	  c_start = document.cookie.indexOf(c_name + "=");
+	  if (c_start != -1) {
+		c_start = c_start + c_name.length + 1;
+		c_end = document.cookie.indexOf(";", c_start);
+		if (c_end == -1) {
+		  c_end = document.cookie.length;
+		}
+		return unescape(document.cookie.substring(c_start, c_end));
+	  }
+	}
+	return "";
+  }
+
+
+
+
+
+
 $(document).ready(function(){
 	$("html").addClass('js');
 
@@ -63,8 +112,8 @@ $(document).ready(function(){
 	}
 
 	// Cookie bar
-	$(".js-cookie-bar-close").click(function(){
-		$(".cookie-bar").hide();
-	});
+	// $(".js-cookie-bar-close").click(function(){
+	// 	$(".cookie-bar").hide();
+	// });
 
 });
